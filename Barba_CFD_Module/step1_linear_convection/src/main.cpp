a52e7f616ea9ff169d82a77ed4919c2a90654383
tylera277 <tylera277@gmail.com>
2023-07-04 15:14:30 -0600

    simplified file printing for the time being

--- Barba_CFD_Module/step1_linear_convection/src/main.cpp
+++ Barba_CFD_Module/step1_linear_convection/src/main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 #include <Eigen/Dense>
 
 #include "supplemental_files/supplemental_files.hpp"
@@ -19,19 +20,26 @@ int main()
     const double nx = 40;
     const double dx = 2 / (nx-1);
     double counter = 0;
+    
 
-    const int nt = 25;
-    const int dt = 1;
+    const int nt = 2;
+    const double dt = 0.025;
 
     const int c = 1;
 
     Eigen::VectorXf u( (int(nx)) );
     Eigen::VectorXf un( (int(nx)) );
 
+    Eigen::VectorXf test(1);
+    std::cout << "TEST1: " << test.coeffRef(0) << "\n";
+    test.coeffRef(0) = 5;
+    std::cout << "TEST2: " << test.coeffRef(0) << "\n";
+
     // Initializing the Initial conditions
     while(counter<nx)
     {
-        if(counter>=0.5 && counter <=1)
+        double positionOfPoint = counter * dx;
+        if(positionOfPoint>=0.5 && positionOfPoint <=1)
         {
             u.coeffRef(counter) = 2.0;
 
@@ -47,25 +55,45 @@ int main()
 
 
     // Main loop 
-    for(int i = 0; i < nt; i += dt)
+    for(int i = 0; i < nt; i += 1)
     {
         un = u;
 
         for(int j  = 1; j < nx; j++)
         {
-            u.coeffRef(j) = un.coeffRef(j) - c * ( dt / dx ) * ( un.coeffRef(j) - un.coeffRef(j-1));
+            u.coeffRef(j) = un.coeffRef(j) - (c * ( dt / dx ) ) * ( un.coeffRef(j) - un.coeffRef(j-1));
+
+            std::cout << "New Value: " << u.coeffRef(j) << "\n";
 
         }
+        std::cout << "------------\n"; 
 
 
     }
+
+    /*
+    ** Struggling on getting this to compile for some reason
 
     // Handling the output of the calculated values in order to be plotted with 
     // python in a separate program
     SupplementalFiles sf;
 
     sf.printFilesToCSV(outputFilename, u);
+    */
 
+    std::ofstream output;
+    output.open(outputFilename);
+
+    for(int iterator = 0; iterator < u.size(); iterator++)
+    {
+        output << u.coeffRef(iterator) << ", ";
+
+
+    }
+    output<<"\n";
+
+    // Closing file after having finished printed out to it
+    output.close();
     
 
     return 0;
